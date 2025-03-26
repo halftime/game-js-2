9cd734d76396df782b38356fd7ede06b04977299
Ignc <60786122+halftime@users.noreply.github.com>
2025-03-26 23:46:40 +0100

    added 4 (original) spawnpoints

--- map/wallplot.py
+++ map/wallplot.py
@@ -9,9 +9,14 @@ import json
 # X1, Y1: Top left corner
 # X2, Y2: Bottom right corner
 
-with open('./not_walkables.json', 'r') as f:
+with open(r'./not_walkables.json', 'r') as f:
     rectgls = json.load(f)
 
+# Load spawn points from json
+# (X, Y): Coordinates for spawn points
+with open(r'./spawn_points.json', 'r') as f:
+    spawn_points = json.load(f)
+    
 # Load image
 im = Image.open(r'./xgen_studio-map.png')
 pixels = np.array(im)
@@ -21,6 +26,9 @@ ax.imshow(pixels)
 
 for rect in rectgls:
     ax.add_patch(patches.Rectangle((rect[0], rect[1]), rect[2]-rect[0], rect[3]-rect[1], linewidth=1, edgecolor='white', facecolor='none'))
+
+for point in spawn_points:
+    ax.add_patch(patches.Circle((point[0], point[1]), radius=10, color='white', fill=False, linewidth=2))
 
 plt.savefig('./output_wallplot.png')
 plt.show()
